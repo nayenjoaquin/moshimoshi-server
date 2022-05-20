2b866d6b22688c6da80e64672e8c0a247870c448
Joaquin Nayen <nayenjoaquin@gmail.com>
2022-05-19 20:26:47 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
+from flask_mysqldb import MySQL
+from config import DevelopmentConfig
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config.from_object(DevelopmentConfig)
+db = MySQL(app)
+
+
+@app.route('/getMangas')
+@cross_origin()
+def getMangas():
+    cur = db.connection.cursor()
+    cur.execute('''SELECT * FROM manga''')
+    data = cur.fetchall()
+    return jsonify(data)
+
+
+@app.route('/getMangasByName/<name>')
+@cross_origin()
+def getMangasByName(name):
+    cur = db.connection.cursor()
+    cur.execute('''SELECT * FROM manga
+                WHERE manga.name="{}"'''.format(name))
+    data = cur.fetchall()
+    return jsonify(data)
+
+@app.route('/getManga/<id>')
+@cross_origin()
+def getMangaById(id):
+    cur = db.connection.cursor()
+    cur.execute('''SELECT * FROM manga
+                WHERE manga.id={}'''.format(id))
+    data = cur.fetchall()
+    return jsonify(data)
+
+@app.route('/getMangasNameList')
+@cross_origin()
+def getMangasNameList():
+    cur = db.connection.cursor()
+    cur.execute('''SELECT name, id FROM manga''')
+    data = cur.fetchall()
+    return jsonify(data)
+
+@app.route('/newManga', methods=['POST'])
+def newManga():
+    return 'agregando mangas'
+
+
+@app.route('/deleteManga/<id>')
+def deleteManga(id):
+    return 'eliminando manga'
+
+
+@app.route('/updateManga', methods=['POST'])
+def updateManga():
+    return 'actualizando stock'
+
---
+++ config.py
@@ -0,0 +1,9 @@
+class Config:
+    SECRET_KEY = 'B!1weNAt1T^%kvhUI*S^'
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MYSQL_HOST = '212.1.208.151'
+    MYSQL_USER = 'u331827210_nayenjoaquin'
+    MYSQL_PASSWORD = 'Juackorey.90'
+    MYSQL_DB = 'u331827210_moshimoshi'
